dc75dd1e1d8178bc948280c04471c7b6b4c58d14
jaywcjlove <398188662@qq.com>
2022-08-19 22:45:20 +0800

    chore(ic): update scripts.

--- icons/ic/cpy.mjs
+++ icons/ic/cpy.mjs
@@ -6,10 +6,10 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 ;(async () => {
-  await cpy('data/azure-cds/*.svg', '.cache_svg', { flat: true, cwd: __dirname, rename: (basename) => `cds-${basename}` })
-  await cpy('data/azure-docs/*.svg', '.cache_svg', { flat: true, cwd: __dirname, rename: (basename) => `doc-${basename}` })
-  await cpy('data/azure-icons/*.svg', '.cache_svg', { flat: true, cwd: __dirname, rename: (basename) => `icon-${basename}` })
-  await cpy('data/azure-patterns/*.svg', '.cache_svg', { flat: true, cwd: __dirname, rename: (basename) => `pattern-${basename}` })
-  await cpy('data/azure-logos/*.svg', '.cache_svg', { flat: true, cwd: __dirname, rename: (basename) => `logo-${basename}` })
-  await cpy('data/azure-other/*.svg', '.cache_svg', { flat: true, cwd: __dirname })
+  await cpy('data/azure-cds/*.svg', '.cache_svg', { flat: true, cwd: __dirname, rename: (basename) => `cds-${basename}` });
+  await cpy('data/azure-docs/*.svg', '.cache_svg', { flat: true, cwd: __dirname, rename: (basename) => `doc-${basename}` });
+  await cpy('data/azure-icons/*.svg', '.cache_svg', { flat: true, cwd: __dirname, rename: (basename) => `icon-${basename}` });
+  await cpy('data/azure-patterns/*.svg', '.cache_svg', { flat: true, cwd: __dirname, rename: (basename) => `p-${basename}` });
+  await cpy('data/logos/*.svg', '.cache_svg', { flat: true, cwd: __dirname, rename: (basename) => `logo-${basename}` });
+  await cpy('data/other/out/*.svg', '.cache_svg', { flat: true, cwd: __dirname, rename: (basename) => `o-${basename}` });
 })();
