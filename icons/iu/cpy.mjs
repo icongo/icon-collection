5066b37c9ed5ab4fce10de8774403d9ed057fe30
jaywcjlove <398188662@qq.com>
2022-08-29 14:59:43 +0800

    fix: update iu script.

--- icons/iu/cpy.mjs
+++ icons/iu/cpy.mjs
@@ -6,7 +6,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 ;(async () => {
-  await cpy('data/vectors/**/*.svg', '.cache_svg', {
+  await cpy(['data/vectors/**/*.svg', '!data/vectors/skiff.com/skiff.svg'], '.cache_svg', {
     flat: true,
     cwd: __dirname,
   })
